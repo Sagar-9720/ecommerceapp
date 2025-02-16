a12135449c58ab1f9889b72aed33ca4a223fdad6
Sagar-9720 <106835611+Sagar-9720@users.noreply.github.com>
2025-02-16 13:55:56 +0530

    home screen in between

--- lib/features/personalization/screens/home/home.dart
+++ lib/features/personalization/screens/home/home.dart
@@ -3,6 +3,7 @@ import 'package:ecommerce/common/widgets/custom_shapes/containers/search_contain
 import 'package:ecommerce/common/widgets/texts/section_heading.dart';
 import 'package:ecommerce/features/personalization/screens/home/widgets/home_appbar.dart';
 import 'package:ecommerce/features/personalization/screens/home/widgets/home_categories.dart';
+import 'package:ecommerce/utils/constants/image_strings.dart';
 import 'package:ecommerce/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
 
@@ -14,51 +15,49 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
         body: SingleChildScrollView(
             child: Column(
-              children: [
-                const TPrimaryHeaderContainer(
-                  child: Column(
-                    children: [
+      children: [
+        const TPrimaryHeaderContainer(
+          child: Column(
+            children: [
+              /// Appbar
+              THomeAppBar(),
+              SizedBox(height: TSizes.spaceBtwSections),
 
-                      /// Appbar
-                      THomeAppBar(),
-                      SizedBox(height: TSizes.spaceBtwSections),
+              /// SearchBar
+              TSearchContainer(
+                text: 'Search in store',
+              ),
+              SizedBox(height: TSizes.spaceBtwSections / 2),
 
-                      /// SearchBar
-                      TSearchContainer(
-                        text: 'Search in store',
-                      ),
-                      SizedBox(height: TSizes.spaceBtwSections / 2),
+              /// Categories
+              Padding(
+                padding: EdgeInsets.only(left: TSizes.defaultSpace),
+                child: Column(
+                  children: [
+                    ///Heading
+                    TSectionHeading(
+                      showActionButton: false,
+                      title: 'Popular Categories',
+                      textColor: Colors.white,
+                    ),
+                    SizedBox(height: TSizes.spaceBtwItems),
 
-                      /// Categories
-                      Padding(
-                        padding: EdgeInsets.only(left: TSizes.defaultSpace),
-                        child: Column(
-                          children: [
-
-                            ///Heading
-                            TSectionHeading(
-                              showActionButton: false,
-                              title: 'Popular Categories',
-                              textColor: Colors.white,
-                            ),
-                            SizedBox(height: TSizes.spaceBtwItems),
-
-                            /// Categories
-                            THomeCategories()
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
+                    /// Categories
+                    THomeCategories()
+                  ],
                 ),
+              )
+            ],
+          ),
+        ),
 
-                /// Body
-                Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(TSizes.md)),
-                  child: const Image(image: AssetImage(TImages.promoBanner1)),
-                )
-              ],
-            )));
+        /// Body
+        Container(
+          decoration:
+              BoxDecoration(borderRadius: BorderRadius.circular(TSizes.md)),
+          child: const Image(image: AssetImage(TImages.promoBanner1)),
+        )
+      ],
+    )));
   }
 }
